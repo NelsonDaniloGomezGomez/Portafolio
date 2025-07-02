7b70cdc91af62f18ea7b191dab153c012573e383
Nelson Gómez <nelsondanilogomezgomez@gmail.com>
2025-07-01 22:37:21 -0400

    se arregla conflicto de versiones

---
+++ src/components/Proyectos.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Proyectos</h2>
+    <ul>
+      <li v-for="proyecto in proyectos" :key="proyecto.titulo">
+        <strong>{{ proyecto.titulo }}</strong><br />
+        <button @click="ir(proyecto.url)">🔗 Ver proyecto</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const proyectos = [
+  { titulo: 'Crypto App', url: 'https://misitio.com/crypto' },
+  { titulo: 'ToDo App', url: 'https://misitio.com/todo' },
+  { titulo: 'Mi Tienda Online', url: 'https://misitio.com/tienda' }
+]
+
+function ir(url) {
+  window.open(url, '_blank')
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin: 10px 0;
+}
+button {
+  margin-top: 5px;
+}
+</style>
